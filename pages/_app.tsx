365ef36122c76a4b7f7469c4ca6bf1a5c1037fc3
JavokhirRuziev <javokhirruziev3@gmail.com>
2024-08-21 23:40:33 +0500

    meta tags added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,30 @@ export default function App({ Component, pageProps }: AppProps) {
         <ThemeProvider theme={theme}>
             <Head>
                 <title>Maqsad Invest</title>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '867822678604600');
+              fbq('track', 'PageView');
+            `,
+                    }}
+                />
+                <noscript>
+                    <img
+                        height='1'
+                        width='1'
+                        style={{ display: 'none' }}
+                        src='https://www.facebook.com/tr?id=867822678604600&ev=PageView&noscript=1'
+                    />
+                </noscript>
             </Head>
             <ToastContainer />
             <Component {...pageProps} />
